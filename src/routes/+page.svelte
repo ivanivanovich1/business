8cb79882634718904695b489ffea9ceb79ec3c30
Pavel Bogatõr <pavel.bogator@tptlive.ee>
2023-11-15 10:59:55 +0200

    Add business data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="pt-24">
+  <h1 class="text-5xl font-bold">Summit Solutions Enterprises</h1>
+  <p class="py-5">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sunt
+    voluptates! Consequatur necessitatibus quas amet repellendus saepe,
+    accusamus praesentium facere architecto placeat esse dolorum. Expedita
+    nesciunt omnis praesentium quos commodi!
+  </p>
+  <a href="#contact" class="btn btn-primary">GET STARTED!</a>
+</section>
+
+<section class="text-center">
+  <h2 class="font-bold text-4xl mb-3">Science is on our side</h2>
+  <p>
+    Studies show that if you won't give us your money, <span class="font-bold"
+      >you won't be able to succeed</span
+    >
+  </p>
+</section>
+
+<section class="grid gap-5 md:grid-cols-2">
+  <div>
+    <h2 class="text-2xl font-bold">People LOVE our service</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
+      consectetur, qui nostrum iste impedit aliquid nemo totam, assumenda
+      necessitatibus animi consequatur quisquam veritatis officia magni soluta
+      excepturi, dolor vel optio.
+    </p>
+  </div>
+  <img
+    class="rounded-xl"
+    src="https://media.istockphoto.com/id/1369510216/photo/carefree-beautiful-latin-woman-relaxing-on-beach.jpg?s=612x612&w=0&k=20&c=PAwWl2-RrpAzO0bb1oPVmKX3JD-IzX-Wl54i6HpcZsA="
+    alt="happy woman"
+  />
+</section>
+
+<section id="contact">
+  <h2 class="font-bold text-2xl">Contact us</h2>
+  <div class="form-control">
+    <label class="label">
+      <span class="label-text">Your credit card data</span>
+    </label>
+    <textarea class="textarea textarea-bordered h-24" placeholder="Bio" />
+  </div>
+</section>
